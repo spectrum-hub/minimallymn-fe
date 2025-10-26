190785ec88ed7d8115f8393230dfafc1cfb0714e
Batgerel <b.batgerel2016@gmail.com>
2025-10-26 15:08:30 +0800

    brandName

--- src/components/BrandBadge/index.tsx
+++ src/components/BrandBadge/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
-import { ProductBrand } from "../types/Products";
+import { ProductBrand } from "../../types/Products";
+import style from "./style.module.css";
 
 interface Props {
   brand?: ProductBrand;
@@ -11,25 +12,15 @@ const BrandBadge: FC<Props> = ({ brand, isViewBrand }) => {
   }
 
   if (brand?.logo && brand.logo.length > 10) {
-    const classBrandName =
-      "w-20 md:w-24 min-h-14 object-contain border border-gray-200 rounded-lg p-2 mb-2 ";
     return (
       <img
         src={`data:image/png;base64,${brand?.logo}`}
         alt={`${brand?.name} Logo`}
-        className={classBrandName}
+        className={style.brandLogo}
       />
     );
   }
-  return (
-    <h2
-      className="text-lg 
-      border border-gray-100 rounded-lg max-w-28 text-center
-      font-semibold text-gray-900 font-sans "
-    >
-      {brand?.name}
-    </h2>
-  );
+  return <h2 className={style.brandName}>{brand?.name}</h2>;
 };
 
 export default BrandBadge;
